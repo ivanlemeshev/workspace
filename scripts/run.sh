cdc845c3777e55c188ca32d0f4455731b0415eff
Ivan Lemeshev <ivanlemeshev@users.noreply.github.com>
2024-09-29 19:20:44 +0300

    Update workspace (#8)

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,20 +2,17 @@
 
 set -e
 
-source ./scripts/functions.sh
+source "$(dirname "$0")/config.sh"
+source "$(dirname "$0")/functions/print_header.sh"
 
 print_header "Runnin Ubuntu in a Docker contasiner"
 
-image_name="my/workspace"
-container_name="my_workspace"
-hostname="workspace"
-
-if [[ -z "$(docker images -q ${image_name} 2> /dev/null)" ]]; then
-    print_header "Building image: ${image_name}"
+if [[ -z "$(docker images -q ${image_name} 2>/dev/null)" ]]; then
+    print_header "Building the image: ${image_name}"
     docker build -t "${image_name}" -f Dockerfile .
 fi
 
-if [[ -z "$(docker ps -aq -f name=${container_name} 2> /dev/null)" ]]; then
+if [[ -z "$(docker ps -aq -f name=${container_name} 2>/dev/null)" ]]; then
     print_header "Running the new container: ${container_name}"
     docker run -it -h "${hostname}" --name "${container_name}" "${image_name}"
 else
